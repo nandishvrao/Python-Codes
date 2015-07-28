e6e1399d757f34476dc31300509515fcd0eef133
nandishvrao <nandishvrao@gmail.com>
2015-07-28 23:38:24 +0530

    Create input_from_user.py

---
+++ input_from_user.py
@@ -0,0 +1,15 @@
+'''Author:Nandish V
+nandishvrao@gmail.com
+this code works fine in python 3.4.3'''
+#triple quotes are used to comments multiple lines
+'''we have two types of getting input from the user
+1)input(for new version)
+2)raw_input(for older version)'''
+
+first=int(input("enter the first number:"))
+second=int(input("enter the second number:"))
+
+add=first+second
+
+print("the sum of two numbers entered is:",add)
+
